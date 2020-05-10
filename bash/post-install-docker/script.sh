6c20b1246df3eabfe49d38c91c4b3ea51e0691ec
Memory Clutter <memclutter@gmail.com>
2020-05-10 12:30:33 +0300

    Add zsh

--- bash/post-install-docker/script.sh
+++ bash/post-install-docker/script.sh
@@ -38,3 +38,8 @@ systemctl start docker
 # Docker compose
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# Oh my zsh :)
+apt install zsh -yq
+chsh -s $(which zsh) ubuntu
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
